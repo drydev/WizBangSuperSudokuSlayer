a2358ceb6b8649a0809951794dbaba3326d0eb21
Nick Dryden <nick.dryden@veritec.com.au>
2021-02-24 15:11:21 +1100

    Last merge seemed to fail and delete classes, fixed in this commit

--- PrintArray.cs
+++ PrintArray.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace WizBangSuperSudokuSlayer
 {
-    class Print2DArray
+    class PrintArray
     {
+        public static void Print2DArray<T>(T[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
